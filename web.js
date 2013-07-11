e6abe0e88b83112335ae045740cf0f201fc6e114
JM <hello2world@outlook.com>
2013-07-11 05:12:25 +0000

    Added web.js

--- web.js
+++ web.js
@@ -1,12 +1,22 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
+//Modify web.js again, except this time have it read "index.html" from disk and use that data in place of "Hello World 2!"
+//var buffer = new Buffer("I'm a string!", "utf-8");
+//fs.readFileSync(index.html); //return contents of 1st argument, returning a buffer
+
+var fs = require('fs');
+var buffer = fs.readFileSync('index.html','utf8');
+//var buffer = fs.readFileSync('index.html');
+//buffer.toString("utf-8");
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send('Hello World 2!; Buffer:'+buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+  console.log(buffer.toString());
 });
